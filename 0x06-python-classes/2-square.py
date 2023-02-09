7df9bddfeaa1fff214da643ab2d883bfce84ad89
Njagi254 <samuelkamau09@gmail.com>
2023-02-09 23:34:52 +0300

    t2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,48 @@
 #!/usr/bin/python3
 
+
 class Square:
+    """A class that defines a square.
+
+    Attributes:
+        __size (int): Private instance attribute.
+    """
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
+        """Initialize a square with an optional size.
+
+        Args:
+            size (int): Size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
-    def area(self):
-        return self.__size ** 2
+    @property
+    def size(self):
+        """int: Size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size of the square.
+
+        Args:
+            value (int): Size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
