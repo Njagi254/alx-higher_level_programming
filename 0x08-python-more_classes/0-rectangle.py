197187d5e2ef0e2cdbc89759c7483cb8edd1bd73
Njagi254 <samuelkamau09@gmail.com>
2023-02-14 08:41:35 +0300

    t0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 
+# 0-rectangle module
+"""
+This module contains a class Rectangle
+
+>>> Rectangle = __import__('0-rectangle').Rectangle
+
+>>> my_rectangle = Rectangle()
+>>> print(type(my_rectangle))
+<class '0-rectangle.Rectangle'>
+
+>>> print(my_rectangle.__dict__)
+{}
+
+"""
+
 
 class Rectangle:
-    """
-    A class used to represent a Rectangle
-
-    Attributes
-    ----------
-    None
-
-    Methods
-    -------
-    None
-    """
+    """This is an empty class"""
     pass
