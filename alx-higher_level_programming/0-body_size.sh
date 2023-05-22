1c4d35459b708593613ef2e0c2a67dbaf6d5b804
Njagi254 <samuelkamau09@gmail.com>
2023-05-22 14:18:57 -0700

    modified

--- alx-higher_level_programming/0-body_size.sh
+++ alx-higher_level_programming/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, displays the size of the body of the response
+# Script that takes a URL, sends a GET request to it, and displays the size of the response body
 curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
